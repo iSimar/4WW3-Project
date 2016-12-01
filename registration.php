2713f0dbc00fff572876aef2f8d921c1af39a248
Simar Singh <hello@simarsingh.com>
2016-12-01 11:17:27 -0500

    registeration is done

--- registration.php
+++ registration.php
@@ -25,76 +25,295 @@
             <a href="registration.php" class="sign-up-link active-link">Signup</a>
         </div>
         <div class="content">
+            <?php
+                $processed = $_SERVER['REQUEST_METHOD'] === 'POST';
+                $accountCreated = false;
+                
+                $username = $_POST['username'];
+                $fullName = $_POST['fullName'];
+                $birthDate = $_POST['birthDate'];
+                $email = $_POST['email'];
+                $phone = $_POST['phone'];
+                $password = $_POST['password'];
+                $confirmPassword = $_POST['confirmPassword'];
+
+                // print_r($_POST);
+                if(
+                    $processed &&
+                    $username !=='' &&
+                    $fullName !=='' &&
+                    $birthDate !== '' &&
+                    $email !== '' &&
+                    $phone !== '' &&
+                    $password !== '' &&
+                    $confirmPassword !== '' &&
+                    preg_match('/^\d{1,2}\/\d{1,2}\/\d{4}$/', $birthDate) &&
+                    preg_match('/^[a-zA-Z\s]+$/', $fullName) &&
+                    preg_match('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $email) &&
+                    preg_match('/^[0-9]+$/', $phone) &&
+                    strlen($phone)==10 &&
+                    $password == $confirmPassword
+                   ) {
+                    include 'connect.php';
+                    $count_username_query = $db->query("SELECT count(*) FROM `users` WHERE username='$username'");
+                    if($count_username_query->fetchColumn()==0){
+                        $username_exists = false;
+                        $md5_password = md5($password);
+                        $insert_user_query = $db->query("INSERT INTO `users`(`username`, `password`, `birth_date`, `email`, `phone_number`, `full_name`) VALUES ('$username','$md5_password','$birthDate','$email','$phone','$fullName')");
+                        $accountCreated = true;
+                    }
+                    else{
+                        $username_exists = true;
+                    }
+                    $insert_user_query = null;
+                    $count_username_query = null;
+                    $db = null;
+              }
+              if($processed && $accountCreated){
+            ?>
+                <div class="inner-content">
+                    <h1 class="content-title">
+                        Congratulations 
+                    </h1>
+                    <div class="small-sweeper"></div>
+                    <div class="blue-box login-box">
+                        <br/>
+                        <h2>You are a member now! Thank you for signing up. You can login now.</h2>
+                        <br/>
+                        <a class="button" href="login.php">Login Now</a>
+                    </div>
+                </div>
+            <?php
+                }
+                else {
+            ?>
             <div class="inner-content">
                 <h1 class="content-title">
                     Ready to join?
                 </h1>
                 <div class="small-sweeper"></div>
                 <div class="blue-box login-box">
+                    <form action="registration.php" method="POST">
                     <br/>
-                    <input type="text" class="textbox text-box-full-width" placeholder="Username"/>
+                    <input type="text" name="username" class="textbox text-box-full-width" placeholder="Username" id="username" value="<?php echo $username ?>"/>
+                    <?php 
+                        if($processed && $username && $username_exists == 1){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Username already exists!
+                            </div>
+                            <div class="error-message-box-description">
+                                <?php echo $username ?> is already in use, please try using a different username.
+                            </div>
+                        </div>
+                    <?php
+                        }
+                        else if($processed && !$username){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Required field
+                            </div>
+                            <div class="error-message-box-description">
+                                Username is a required field
+                            </div>
+                        </div>
+                    <?php
+                        }
+                    ?>
                     <div class="small-sweeper"></div>
-                    <input type="text" class="textbox text-box-full-width" placeholder="Full Name" id="fullName"/>
-                    <div class="error-message-box" id="fullNameRegisterErrorBox">
-                        <div class="error-message-box-title" id="fullNameRegisterErrorBoxTitle">
-                            Error Title
+                    <input type="text" class="textbox text-box-full-width" name="fullName" placeholder="Full Name" id="fullName" value="<?php echo $fullName ?>"/>
+                    <?php 
+                        if($processed && $fullName && !preg_match('/^[a-zA-Z\s]+$/', $fullName)){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Must be alphabetic
+                            </div>
+                            <div class="error-message-box-description">
+                                '<?php echo $fullName ?>' contains other characters than letters.
+                            </div>
+                        </div> 
+                    <?php
+                        }
+                        else if($processed && !$fullName){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Required field
+                            </div>
+                            <div class="error-message-box-description">
+                                Full name is a required field
+                            </div>
                         </div>
-                        <div class="error-message-box-description" id="fullNameRegisterErrorBoxDesc">
-                            This is the explaination of the error.
-                        </div>
-                    </div>
+                    <?php
+                        }
+                    ?>
                     <div class="small-sweeper"></div>
-                    <input type="text" class="textbox text-box-full-width" placeholder="Birth Date (MM/DD/YYYY)" id="birthDate"/>
-                    <div class="error-message-box" id="birthDateRegisterErrorBox">
-                        <div class="error-message-box-title" id="birthDateRegisterErrorBoxTitle">
-                            Error Title
+                    <input type="text" class="textbox text-box-full-width" placeholder="Birth Date (MM/DD/YYYY)" id="birthDate" name="birthDate" value="<?php echo $birthDate ?>"/>
+                    <?php 
+                        if($processed && $birthDate && !preg_match('/^\d{1,2}\/\d{1,2}\/\d{4}$/', $birthDate)){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Must be in date format
+                            </div>
+                            <div class="error-message-box-description">
+                                '<?php echo $birthDate ?>' is NOT in MM/DD/YYYY date format.
+                            </div>
+                        </div> 
+                    <?php
+                        }
+                        else if($processed && !$birthDate){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Required field
+                            </div>
+                            <div class="error-message-box-description">
+                                Birth date is a required field
+                            </div>
                         </div>
-                        <div class="error-message-box-description" id="birthDateRegisterErrorBoxDesc">
-                            This is the explaination of the error.
-                        </div>
-                    </div>
+                    <?php
+                        }
+                    ?>
                     <div class="small-sweeper"></div>
-                    <input type="email" class="textbox text-box-full-width" placeholder="Email" id="email"/>
-                    <div class="error-message-box" id="emailRegisterErrorBox">
-                        <div class="error-message-box-title" id="emailRegisterErrorBoxTitle">
-                            Error Title
+                    <input type="email" class="textbox text-box-full-width" placeholder="Email" id="email" name="email" value="<?php echo $email ?>"/>
+                    <?php 
+                        if($processed && $email && !preg_match('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $email)){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Must be in date format
+                            </div>
+                            <div class="error-message-box-description">
+                                '<?php echo $email ?>' is NOT in MM/DD/YYYY date format.
+                            </div>
+                        </div> 
+                    <?php
+                        }
+                        else if($processed && !$email){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Required field
+                            </div>
+                            <div class="error-message-box-description">
+                                Email is a required field
+                            </div>
                         </div>
-                        <div class="error-message-box-description" id="emailRegisterErrorBoxDesc">
-                            This is the explaination of the error.
-                        </div>
-                    </div>
+                    <?php
+                        }
+                    ?>
                     <div class="small-sweeper"></div>
-                    <input type="text" class="textbox text-box-full-width" placeholder="Phone Number (ex: 4161234567)" id="phone"/>
-                    <div class="error-message-box" id="phoneRegisterErrorBox">
-                        <div class="error-message-box-title" id="phoneRegisterErrorBoxTitle">
-                            Error Title
+                    <input type="text" class="textbox text-box-full-width" placeholder="Phone Number (ex: 4161234567)" id="phone" name="phone" value="<?php echo $phone ?>"/>
+                    <?php 
+                        if($processed && $phone && !preg_match('/^[0-9]+$/', $phone)){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Must be numeric
+                            </div>
+                            <div class="error-message-box-description">
+                                '<?php echo $phone ?>' contains other characters than numbers.
+                            </div>
+                        </div> 
+                    <?php
+                        }
+                        else if($processed && $phone && strlen($phone)!=10){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Invalid phone number
+                            </div>
+                            <div class="error-message-box-description">
+                                Phone number must be 10 digits long
+                            </div>
                         </div>
-                        <div class="error-message-box-description" id="phoneRegisterErrorBoxDesc">
-                            This is the explaination of the error.
+                    <?php
+                        }
+                        else if($processed && !$phone){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Required field
+                            </div>
+                            <div class="error-message-box-description">
+                                Phone number is a required field
+                            </div>
                         </div>
-                    </div>
+                    <?php
+                        }
+                    ?>
                     <div class="small-sweeper"></div>
-                    <input type="password" class="textbox text-box-full-width" placeholder="Password"/>
+                    <input type="password" class="textbox text-box-full-width" placeholder="Password" id="password" name="password"/>
+                    <?php 
+                        if($processed && $password != $confirmPassword){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Passwords did not match
+                            </div>
+                            <div class="error-message-box-description">
+                                Password and confirm password field must match.
+                            </div>
+                        </div> 
+                    <?php
+                        }
+                        else if($processed && !$password){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Required field
+                            </div>
+                            <div class="error-message-box-description">
+                                Password is a required field
+                            </div>
+                        </div>
+                    <?php
+                        }
+                    ?>
                     <div class="small-sweeper"></div>
-                    <input type="password" class="textbox text-box-full-width" placeholder="Confirm Password"/>
+                    <input type="password" class="textbox text-box-full-width" placeholder="Confirm Password" id="confirmPassword" name="confirmPassword"/>
+                    <?php
+                        if($processed && !$confirmPassword){
+                    ?>
+                        <div class="error-message-box" style="display:block;">
+                            <div class="error-message-box-title">
+                                Required field
+                            </div>
+                            <div class="error-message-box-description">
+                                Confirm password is a required field
+                            </div>
+                        </div>
+                    <?php
+                        }
+                    ?>
                     <div class="small-sweeper"></div>
                     <input type="checkbox" name="terms">
                     <span class="terms-line">
                         I accept the 
                     </span><a href="#" class="small-link">Terms and Conditions</a>
                     <div class="small-sweeper"></div>
-                    <a class="button" id="registerButton">Register</a>
+                    <input type="submit" class="button" value="Register"/>
+                    <!--<a class="button" id="registerButton">Register</a>-->
                     <div class="small-sweeper"></div>
+                </form>
                 </div>
                 <div class="large-sweeper"></div>
                 <div class="large-sweeper"></div>
             </div>
+            <?php
+                }
+            ?>
         </div>
         <div class="footer"><!-- starting footer -->
             <h5 class="footer-text"> <!-- starting footer-text h5 tag -->
                 All rights reserved, 2016. Simarpreet Singh. Made in Hamilton, ON.
             </h5><!-- ending footer-text h5 tag -->
         </div><!-- ending footer -->
-        <script src="./js/formValidation.js" type="text/javascript"></script>
+        <script src="./js/requests.js" type="text/javascript"></script>
+        <!-- <script src="./js/formValidation.js" type="text/javascript"></script> -->
     </body><!-- ending body tag -->
 </html><!-- ending html tag -->
