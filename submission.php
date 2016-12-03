a3526e976e518d0d9ba6ad5222e333a19580c02d
Simar Singh <hello@simarsingh.com>
2016-12-03 11:59:04 -0500

    done submission

--- submission.php
+++ submission.php
@@ -15,6 +15,32 @@
         <body> <!-- starting body tag -->
         <?php include 'header.php' ?>
         <div class="content">
+            <?php
+                $post_request = $_SERVER['REQUEST_METHOD'] === 'POST';
+                $hotspotCreated = false;
+                $name = $_POST['name'];
+                $longitude = $_POST['longitude'];
+                $latitude = $_POST['latitude'];
+                $url = $_POST['url'];
+                $rating = $_POST['rating'];
+                $longitudeIsNumeric = preg_match('/^\-?\d+(\.\d+)?$/', $longitude);
+                $latitudeIsNumeric = preg_match('/^\-?\d+(\.\d+)?$/', $latitude);
+                $validUrl = preg_match('/^(http:\/\/|https:\/\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$/', $url);
+                if(
+                    $post_request &&
+                    $name != '' &&
+                    $latitude != '' &&
+                    $longitude != '' &&
+                    $longitudeIsNumeric &&
+                    $latitudeIsNumeric &&
+                    ($url==='' || $validUrl)
+                ){
+                    $ratingNum = substr($rating, 0, 5);
+                    include 'connect.php';
+                    $insert_hotspot_query = $db->query("INSERT INTO `hotspots`(`name`, `longitude`, `latitude`, `website`, `rating`, `imageURL`) VALUES ('$name', '$longitude','$latitude', '$url', '$ratingNum','')");
+                    $hotspotCreated = true;
+                }
+            ?>
             <div class="inner-content">
                 <h1 class="content-title">
                     Found a wifi hotspot?
@@ -24,27 +50,76 @@
                 </h2>
                 <div class="small-sweeper"></div>
                 <div class="blue-box submit-box">
+                    <form action="submission.php" method="POST">
+                    <h2>Name</h2>
+                    <input type="text" class="textbox text-box-full-width" placeholder="eg. Starbucks, thode library, pizza pizza" name="name" id="name" value="<?php echo $name; ?>"/>
+                    <?php
+                        if($post_request && $name ===''){
+                    ?>
+                    <div class="error-message-box" style="display:block;">
+                        <div class="error-message-box-title">
+                            Required field
+                        </div>
+                        <div class="error-message-box-description">
+                            Name is a required field
+                        </div>
+                    </div>
+                    <?php
+                        }
+                    ?>
+                    <br/>
                     <h2>Location</h2>
-                    <input type="text" class="textbox location-textbox" placeholder="Longitude" id="longitudeTextbox"/>
-                    <input type="text" class="textbox location-textbox" placeholder="Latitude" id="latitudeTextbox"/>
+                    <input type="text" class="textbox location-textbox" placeholder="Longitude" name="longitude"id="longitudeTextbox" value="<?php echo $longitude; ?>"/>
+                    <input type="text" class="textbox location-textbox" placeholder="Latitude" name="latitude" id="latitudeTextbox" value="<?php echo $latitude; ?>"/>
+                    <?php
+                        if($post_request && ($longitude === '' || $latitude === '')){
+                    ?>
+                    <div class="error-message-box" style="display:block;">
+                        <div class="error-message-box-title">
+                            Required fields
+                        </div>
+                        <div class="error-message-box-description">
+                            Longitude and Latitude are required
+                        </div>
+                    </div>
+                    <?php
+                        }
+                        elseif($post_request && !($longitudeIsNumeric && $latitudeIsNumeric)){
+                    ?>
+                    <div class="error-message-box" style="display:block;">
+                        <div class="error-message-box-title">
+                            Invalid coordinates format
+                        </div>
+                        <div class="error-message-box-description">
+                            Longitude and Latitude are required to be numeric values
+                        </div>
+                    </div>
+                    <?php
+                        }
+                    ?>
                     <br/>
                     <a class="small-link underlined" id="useCurrentLocation" onclick="onClickUseCurrentLocation()" style="text-align: left;">
                         <h3>Use Current Location</h3>
                     </a>
                     <h2>Website (Optional)</h2>
-                    <input type="text" class="textbox text-box-full-width" placeholder="http://example.com" id="url"/>
-                    <div class="error-message-box" id="urlSubmissionErrorBox">
-                        <div class="error-message-box-title" id="urlSubmissionErrorBoxTitle">
-                            Error Title
+                    <input type="text" class="textbox text-box-full-width" placeholder="http://example.com" name="url" id="url" value="<?php echo $url; ?>"/>
+                    <?php
+                        if($post_request && $url!=='' && !$validUrl){
+                    ?>
+                    <div class="error-message-box" style="display:block;">
+                        <div class="error-message-box-title">
+                            Invalid url format
                         </div>
-                        <div class="error-message-box-description" id="urlSubmissionErrorBoxDesc">
-                            This is the explaination of the error.
+                        <div class="error-message-box-description">
+                            '<?php echo $url; ?>' is not a valid url. (eg. https://google.com)
                         </div>
                     </div>
+                    <?php
+                        }
+                    ?>
                     <br/>
                     <h2>Rating</h2>
-                    <select class="dropdown submit-dropdown">
-                      <option selected>Any</option>
+                    <select class="dropdown submit-dropdown" name="rating">
                       <option>5 Stars</option>
                       <option>4 Stars</option>
                       <option>3 Stars</option>
@@ -57,9 +132,10 @@
                     <input type="file" class="select-image" name="image" accept="image/*">
                     <br/>
                     <div class="small-sweeper"></div>
-                    <a class="button" id="submitButton">Submit</a>
+                    <input type="submit" class="button" value="Submit"/>
                     <br/>
                     <br/>
+                    </form>
                 </div>
                 <div class="large-sweeper"></div>
                 <div class="large-sweeper"></div>
